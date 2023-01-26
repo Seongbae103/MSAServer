a1d7650aee1d1a424accedce62786f7124f3a528
Seongbae103 <seongbae103@gmail.com>
2023-01-26 17:08:51 +0900

    01-26 AWS RDS

---
+++ FastAPIServer/app/env.py
@@ -0,0 +1,7 @@
+USERNAME = "mydb"
+PASSWORD = "rootroot"
+HOSTNAME = "mydb.cmbubf06nq4s.ap-northeast-2.rds.amazonaws.com" # "localhost"
+DATABASE = "mydb"
+PORT = 3306
+CHARSET = "utf8"
+DB_URL = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}"
